7cf64cc8a6607d4f9e7bcfa841c700efb13c43fa
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2024-01-23 08:35:44 -0300

    integrated terminal in vscode

--- storefront/settings.py
+++ storefront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qw&z%#0$pz!x-5r9^r*tfu)9vpvs-nl#^x(--s9e8r26g8beoq'
+SECRET_KEY = 'django-insecure-mu$-+!!r4=0j2y+i97i(iwxc(+qh@-!3^*$vw$3x&rkxim4xpf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
