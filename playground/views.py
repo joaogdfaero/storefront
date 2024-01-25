b18146a85437e8dd29a902f45a183b685082c6e2
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2024-01-25 09:03:58 -0300

    view and template hello

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Request -> Response
+# Request handler
+# action (Rails) -> view (Django)
+
+def say_hello(request):
+    return HttpResponse('Hello World')
+    
